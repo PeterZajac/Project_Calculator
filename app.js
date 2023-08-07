7fcaaed75957a1427cf244d6b4afd0ee3e27bb82
PeterZajac <zajacp011@gmail.com>
2023-08-07 21:45:01 +0200

    validation chceck

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ buttons.forEach((button) => {
     const buttonValue = button.value;
 
     if (buttonValue === "clear") {
-      input = "";
+      input = "-";
       displayInput.innerText = " ";
       displayResult.innerText = "  ";
     } else if (buttonValue === "equal") {
@@ -19,19 +19,39 @@ buttons.forEach((button) => {
       displayInput.innerText = " ";
       input = displayResult.innerText;
     } else {
-      input += buttonValue;
-      displayInput.innerText = input;
+      // Overenie vstupu pred pridaním
+      if (validInput(buttonValue, input)) {
+        input += buttonValue;
+        displayInput.innerText = input;
+      }
     }
   });
 });
 
-// if (
-//   displayInput.innerText === "+" ||
-//   displayInput.innerText === "-" ||
-//   displayInput.innerText === "*" ||
-//   displayInput.innerText === "/"
-// ) {
-//   displayInput.classList.add("operator");
-// } else {
-//   displayInput.classList.remove("operator");
-// }
+// Funkcia na overenie vstupu
+function validInput(buttonValue, currentInput) {
+  // Zabezpečenie, aby nedochádzalo k viacnásobným desatinným bodkám
+  if (buttonValue === "." && currentInput.includes(".")) {
+    return false;
+  }
+  // Zabezpečenie, aby sa príklad nezačínal s * /
+  if ((buttonValue === "*" || buttonValue === "/") && currentInput === "") {
+    return false;
+  }
+  // Zabezpečenie, aby sa za sebou nemohli nachádzať dve znamienka
+  if (
+    currentInput.length > 0 && // kontrola či je vstup dlhší ako 0
+    isOperator(currentInput[currentInput.length - 1]) && // kontrola či posledný znak vstupu je operátor
+    isOperator(buttonValue) // kontrola či je aktuálny znak operátor
+  ) {
+    return false;
+  }
+  return true;
+}
+
+// Zabezpečenie, aby sa za sebou nemohli nachádzať dve znamienka
+
+// Funkcia na kontrolu, či je znak operátor (+, -, *, /)
+function isOperator(char) {
+  return char === "+" || char === "-" || char === "*" || char === "/";
+}
