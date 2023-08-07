015204447bd35b8090f0ce427eb98755f39c6e7a
PeterZajac <zajacp011@gmail.com>
2023-08-07 18:14:40 +0200

    funkcna kalkulačka bez fixov

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 const buttons = document.querySelectorAll("button");
+const displayInput = document.querySelector(".display");
+const displayResult = document.querySelector(".result");
+
+let input = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonValue = button.value;
+
+    if (buttonValue === "clear") {
+      input = "";
+      displayInput.innerText = " ";
+      displayResult.innerText = "  ";
+    } else if (buttonValue === "equal") {
+      const result = eval(input);
+      const roundedResult = parseFloat(result.toFixed(3));
+      displayResult.innerText = roundedResult;
+      displayInput.innerText = " ";
+      input = displayResult.innerText;
+    } else {
+      input += buttonValue;
+      displayInput.innerText = input;
+    }
+  });
+});
+
+// if (
+//   displayInput.innerText === "+" ||
+//   displayInput.innerText === "-" ||
+//   displayInput.innerText === "*" ||
+//   displayInput.innerText === "/"
+// ) {
+//   displayInput.classList.add("operator");
+// } else {
+//   displayInput.classList.remove("operator");
+// }
